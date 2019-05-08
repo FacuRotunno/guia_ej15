21fd0e1cd37ce23fdd7c38a9a40d14269be9cb35
Facundo Rotunno <facu.rotunno@gmail.com>
2019-05-08 20:22:45 -0300

    Calculo de un viaje

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+    void main () {
+
+        float km;
+        printf ("Cuantos Kilometros recorre: ");
+        scanf("%f",&km);
+        system("cls");
+        printf("El total seria de: $%f", km = km * 10.50);
+
+
+    }
